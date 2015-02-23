5d885731522c8539edd5e5fc7f49dd73353995e9
Brett Leibowitz <LEIBO@Bretts-MacBook-Pro.local>
2015-02-23 14:09:35 -0500

    fixed up docs style and routing

--- js/app.js
+++ js/app.js
@@ -19,7 +19,15 @@ var movieFilter = angular.module('movieFilterApp',
     })
     .state('docs', {
       url: '/docs',
+      templateUrl: 'docs/index.html',
+    })
+    .state('appDocs', {
+      url: '/appDocs',
       templateUrl: 'docs/js/app.js.html',
+    })
+    .state('homeDocs', {
+      url: '/homeDocs',
+      templateUrl: 'docs/js/home.js.html',
     })
 });
 
