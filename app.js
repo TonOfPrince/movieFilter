c26a92899ebca088b025c244e0726c0797455430
Brett Leibowitz <LEIBO@Bretts-MacBook-Pro.local>
2015-02-23 12:49:24 -0500

    cleaned up some code and comments

--- app.js
+++ app.js
@@ -2,72 +2,23 @@ var movieFilter = angular.module('movieFilterApp', [])
 
 // controller for filtering movies
 .controller('MovieFilterCtrl', function($scope, Movies) {
+  // extend the factory return to the controller
   angular.extend($scope, Movies);
 })
 
-// factoring for storig movie information
+// factory for requesting movie information
 .factory('Movies', function() {
-  // movie object
-  // var movies = { "movies": [{
-  //     "title": "The Dark Knight",
-  //     "categories": ["Action","Superhero"],
-  //     "year": 2008
-  //   },
-  //   {
-  //     "title": "Star Wars",
-  //     "categories": ["Action"],
-  //     "year": 1977
-  //   },
-  //   {
-  //     "title": "Sleepless In Seattle",
-  //     "categories": ["Romance", "Comedy"],
-  //     "year": 1993
-  //   },
-  //   {
-  //     "title": "The Lion King",
-  //     "categories": ["Children"],
-  //     "year": 1994
-  //   },
-  //   {
-  //     "title": "Toy Story",
-  //     "categories": ["Children"],
-  //     "year": 1995
-  //   },
-  //   {
-  //     "title": "The Shawshank Redemption",
-  //     "categories": ["Drama"],
-  //     "year": 1994
-  //   },
-  //   {
-  //     "title": "Gravity",
-  //     "categories": ["Drama"],
-  //     "year":2013
-  //   },
-  //   {
-  //     "title": "Independence Day",
-  //     "categories": [],
-  //     "year": 1996
-  //   },
-  //   {
-  //     "title": "Lord of The Rings: The Two Towers",
-  //     "categories": ["Action"],
-  //     "year": 2002
-  //   },
-  //   {
-  //     "title": "Frozen",
-  //     "categories": ["Children"],
-  //     "year": 2014
-  //   }]
-  // }
+  // call to the server to get the movies
   $http.get('/movies')
     .success(function(data, status, headers, config) {
         console.log('success');
+        // save the movies
         var movies = data;
-        console.log(data)
       })
       .error(function(data, status, headers, config) {
         console.log('error');
       })
+  // factory return the movies
   return {
     movies: movies
   }
